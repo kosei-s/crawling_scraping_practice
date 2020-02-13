fe8329cbb66cd00ccbb622f7b1f632a809c3aed6
kosei <ryu.go.off@gmail.com>
2020-02-13 22:03:36 +0900

    アプリ完成

--- app1/server.py
+++ app1/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import requests
+import requests, bs4
 
 app = Flask(__name__)
 
@@ -10,14 +10,24 @@ def index():
 @app.route('/list', methods=['POST'])
 def list():
     url = request.form['URL']
-    res = requests.get(url)
+
+    try:
+        res = requests.get(url)
+    except Exception as exc:
+        return '正しいURLを入力してください'
 
     try:
         res.raise_for_status()
     except Exception as exc:
-        render_template('index.html')
+        return '正しいURLを入力してください'
     
-    return render_template('list.html', url=url)
+    soup = bs4.BeautifulSoup(res.text)
+    link_elems = soup.select('a')
+    link_urls = []
+    for i in range(len(link_elems)):
+        link_urls.append(link_elems[i].get('href'))
+    
+    return render_template('list.html', url=url, link_urls=link_urls)
 
 if __name__ == '__main__':
     app.debug = True
